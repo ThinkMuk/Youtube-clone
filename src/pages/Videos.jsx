43f0d4a44939ae53f62d864663f0447b7879d300
Thinkmuk <b2000kang@gmail.com>
2024-02-04 18:25:11 +0900

    feat: title fetch function implemented

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,7 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useParams } from "react-router-dom";
+import VideoCard from "../components/VideoCard";
 
 export default function Videos() {
   const { keyword } = useParams();
-  return <div>Videos {keyword ? `${keyword}` : "👀"}</div>;
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery({
+    queryKey: ["videos", keyword],
+    queryFn: () =>
+      fetch(`/videos/${keyword ? "search" : "popular"}.json`)
+        .then((res) => res.json())
+        .then((data) => data.items),
+  });
+  return (
+    <>
+      <div>Videos {keyword ? `${keyword}` : "👀"}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
