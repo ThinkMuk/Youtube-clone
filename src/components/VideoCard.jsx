75b5add1aab5f654df9c6c632716c3f0963df562
Thinkmuk <b2000kang@gmail.com>
2024-02-09 22:20:45 +0900

    feat: Create VideoDetail component for YouTube playback and info display

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { formatAgo } from "../util/date";
+import { useNavigate } from "react-router-dom";
 
 export default function VideoCard({ video }) {
-  const { title, etag, thumbnails, channelTitle, publishedAt } = video.snippet;
+  const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  const navigate = useNavigate();
   return (
-    <li key={etag}>
-      <img className="w-full" src={thumbnails.medium.url} alt={title} />
+    <li
+      onClick={() => {
+        navigate(`/videos/watch/${video.id}`, { state: { video } });
+      }}
+    >
+      <img
+        className="w-full rounded-lg"
+        src={thumbnails.medium.url}
+        alt={title}
+      />
       <div>
         <p className="font-semibold my-2 line-clamp-2">{title}</p>
         <p className="text-sm opacity-80">{channelTitle}</p>
